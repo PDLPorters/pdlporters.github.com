9242c117f562f645ceda76c1784bca42db7aec62
Daniel Carrera <dcarrera@gmail.com>
2010-05-23 17:16:39 +0200

    Initial commit of pdl-www repository.

---
+++ index.php
@@ -0,0 +1,37 @@
+<?= '<?xml version="1.0" encoding="UTF-8"?>' ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head profile="http://www.w3.org/2005/10/profile">
+	<title>Perl Data Language</title>
+    <link rel="shortcut icon" href="images/favicon.ico"
+    	  type="image/vnd.microsoft.icon"  />
+    <link rel='stylesheet' type='text/css' href='pdl.css' />
+<script type="text/javascript"
+		src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script>
+<script type="text/javascript" src="js/jquery.cycle.all.min.js"></script>
+</head>
+<body class="v01">
+
+<!-- BANNER -->
+<div class="banner-container">
+ <div class="banner"></div>
+</div>
+<!-- END BANNER -->
+
+<!-- SIDE BAR -->
+<div class="sidebar-container">
+  <? include "pages/sidebar.html" ?>
+</div>
+<!-- END SIDE BAR -->
+
+
+<!-- MAIN CONTENT -->
+ <h1>Perl Data Language</h1>
+ <h2>Scientific computing with Perl</h2>
+<div class="main">
+ <? include "pages/home.html" ?>
+</div>
+<!-- END CONTENT -->
+
+</body>
+</html>

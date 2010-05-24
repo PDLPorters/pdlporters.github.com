4b165ea8d74bef366cfaaed2abafce1719219f43
Daniel Carrera <dcarrera@gmail.com>
2010-05-24 08:45:59 +0200

    Improve look & feel. Prepare PHP code for site building.

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+<?
+function randomTheme() {
+	$rand = rand(0,100);
+	if ($rand < 35)
+		return "v01";
+	if ($rand < 80)
+		return"v02";
+	
+	return "v03";
+}
+function formatContents($filename, $title='') {
+	$content = join("\n", file($filename));
+	$content = str_replace('<div name="index">',
+						'<div class="index" name="index">',	$content);
+	$content = str_replace("\n",'[\n]', $content);
+	$content = preg_replace('/^.*<body[^>]*>/', '', $content);
+	$content = preg_replace('/<\/body>.*$/', '', $content);
+	$content = str_replace('[\n]', "\n", $content);
+	$content = preg_replace('/\n+/', "\n", $content);
+	return "<h1 class='title'>$title</h1>$content";
+}
+
+?>
 <?= '<?xml version="1.0" encoding="UTF-8"?>' ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
@@ -10,7 +33,8 @@
 		src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script>
 <script type="text/javascript" src="js/jquery.cycle.all.min.js"></script>
 </head>
-<body class="v01">
+
+<body class="<?= randomTheme() ?>">
 
 <!-- BANNER -->
 <div class="banner-container">
@@ -20,18 +44,20 @@
 
 <!-- SIDE BAR -->
 <div class="sidebar-container">
-  <? include "pages/sidebar.html" ?>
+  <? include "content/sidebar.html" ?>
 </div>
 <!-- END SIDE BAR -->
 
 
 <!-- MAIN CONTENT -->
- <h1>Perl Data Language</h1>
- <h2>Scientific computing with Perl</h2>
 <div class="main">
  <?
- 	$page = preg_match('/^[-_a-zA-Z]$/',$_GET['page'])) ? $_GET['page'] : 'home';
- 	include "content/$page.html";
+ 	$page = 'home';
+ 	if (isset($_GET['page']) && preg_match('/^[-_a-zA-Z0-9]+$/',$_GET['page'])) {
+ 		$page = $_GET['page'];
+ 	}
+ 	
+ 	require_once "content/$page.html";
  ?>
 </div>
 <!-- END CONTENT -->

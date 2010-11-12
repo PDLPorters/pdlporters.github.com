c3c7c29634763529aa4ce60897861720223cd595
Derek Lamb <lambd@users.sourceforge.net>
2010-11-12 20:42:54 +0000

    Changes to pass xhtml validation
    Numerous small problems, like unencoded ampersands, missing alt-text on images, etc.  At least the front page pdl.perl.org now passes validation.  Also incorporated a few changes that had been made to the website put never put into the git repo.

--- index.php
+++ index.php
@@ -1,46 +1,27 @@
 <?
-function formatContents($filename, $title='') {
+function formatDocs($filename, $title='') {
 	$content = file_get_contents($filename);
-	$content = str_replace('<div name="index">',
-						'<div class="index" name="index">',	$content);
 	$content = str_replace("\n",'[\n]', $content);
 	
-	
-	//
-	//  Fix URLs.
-	//
-	$content = str_replace('<a href="./', '<a href="', $content);
-	
-	// PDL man pages.
+	// Fix links to man pages.
 	$content = preg_replace('/<a href="([\/\w]+)\.html(\#?\w*)">([^<]+)<\/a>/',
-							'<a href="?docs=$1&title=$3$2">$3</a>', $content);
+							'<a href="?docs=$1&amp;title=$3$2">$3</a>', $content);
 	
 	// External modules -- link to perldoc.perl.org and hope they have it.
 	$content = preg_replace('/<a href="..\/([\/\w]+)\.html(\#?\w*)">/',
 							'<a href="http://perldoc.perl.org/$1.html$2">', $content);
 	
-	
 	// Remove body tags.
 	$content = preg_replace('/^.*<body[^>]*>/', '', $content);
 	$content = preg_replace('/<\/body>.*$/', '', $content);
-	
-	// Replace <a name="foo"> with ids in all headers.
-	for ($i = 1; $i <= 6; $i++)
-		$content = preg_replace('|<h'.$i.'><a name="([^"]+)">([^<]+)</a></h'.$i.'>|',
-		                         '<h'.$i.' id="$1">$2</h'.$i.'>', $content);
-	
-	// Put "top" links on all headings.
-	$top_link = '<span style="font-size: 50%">[<a href="#top">top</a>]</span>';
-	$content = str_replace('</h1>', " $top_link</h1>", $content);
-	
 	
 	// Finished.
 	$content = str_replace('[\n]', "\n", $content);
 	$content = preg_replace('/\n+/', "\n", $content);
 	return  "
 	<b>See also:</b> <a href='?page=function-ref'>How do I search for a function?</a>
-	<h1 id='top' class='title'>$title</h1>
-	$content
+	<h1 class='title'>$title</h1>
+	<div class='pod'>$content</div>
 	";
 }
 
@@ -48,13 +29,14 @@ function formatContents($filename, $title='') {
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head profile="http://www.w3.org/2005/10/profile">
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 	<title>Perl Data Language</title>
     <link rel="shortcut icon" href="images/favicon.ico"
     	  type="image/vnd.microsoft.icon"  />
     <link rel="stylesheet" type="text/css"
           href="http://www.google.com/cse/style/look/espresso.css" />
     <link rel='stylesheet' type='text/css' href='google-search.css' />
-    <link rel="stylesheet"type="text/css" 
+    <link rel="stylesheet" type="text/css" 
           href="css/jquery-ui-1.8.2.custom.css" />	
     <link rel='stylesheet' type='text/css' href='pdl.css' />
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"
@@ -80,11 +62,11 @@ function formatContents($filename, $title='') {
   <!-- BANNER --> 
   <div class="banner">
     <a href="index.php?page=home">
-      <img src="images/icons/pdl.png" height="79px" width="146px"/>
+      <img src="images/icons/pdl.png" alt="PDL" height="79px" width="146px"/>
     </a>
-	<img src="images/banners/pos-1-opt-<?=rand(0,2)?>.jpg" height="79px" width="278px"/>
-	<img src="images/banners/pos-2-opt-<?=rand(0,2)?>.jpg" height="79px" width="93px"/>
-	<img src="images/banners/pos-4-opt-<?=rand(0,1)?>.jpg" height="79px" width="110px" style="margin-left: 25px"/>
+	<img src="images/banners/pos-1-opt-<?=rand(0,2)?>.jpg" alt="Banner image1" height="79px" width="278px"/>
+	<img src="images/banners/pos-2-opt-<?=rand(0,2)?>.jpg" alt="Banner image2" height="79px" width="93px"/>
+	<img src="images/banners/pos-4-opt-<?=rand(0,1)?>.jpg" alt="Banner image3" height="79px" width="110px" style="margin-left: 25px"/>
     <div id="cse-search-form" style="position: absolute; top: 20px; left: 700px">Loading</div>
   </div> 
   <!-- END BANNER -->
@@ -126,15 +108,16 @@ function formatContents($filename, $title='') {
 	 	require_once "content/".$_GET['page'].".html";
 	 	
 	} elseif (isset($_GET['docs']) && issane($_GET['docs'])) {
-		echo formatContents("PDLdocs/".$_GET['docs'].".html",$_GET['title']);
+		echo formatDocs("PDLdocs/".$_GET['docs'].".html",$_GET['title']);
 		
  	} else {
 	 	require_once "content/home.html";
  	}
   ?>
   </div>
+
   <!-- END CONTENT -->
- 
+
 </body>
 </html>
 

0bb54032fa6363c908a94a5803b0fe2ddfcdd5f9
Joel Berger <joel.a.berger@gmail.com>
2015-01-10 10:30:44 -0600

    sidebar content is now purely javascript

---
+++ content/sidebar.js
@@ -0,0 +1,42 @@
+sidebar = [
+  {
+    title: 'About PDL',
+    pages: [
+      new HomeLink(),
+      new PageLink('Get PDL', 'install'),
+      new PageLink('Screenshots', 'screenshots/index'),
+      new PageLink('PDL Users', 'users'),
+      new PageLink('Mailing Lists', 'mailing-lists'),
+    ],
+  },
+  {
+    title: 'Documentation',
+    pages: [
+      new PageLink('PDL Book', 'FirstSteps'),
+      new DocsLink('FAQ', 'PDL::FAQ', 'Frequently Asked Questions'),
+      new DocsLink('Tutorials', 'PDL::Tutorials'),
+      new DocsLink('Modules', 'PDL::Modules'),
+      new DocsLink('Course', 'PDL::Course'),
+      new DocsLink('Index', 'PDL::Index'),
+    ],
+  },
+  {
+    title: 'Resources',
+    pages: [
+      new PageLink('Demos', 'demos/index'),
+      new WikiLink('Presentations', 'Colloquia'),
+      new WikiLink('PDL Wiki'),
+      new WikiLink('External Libs', 'Libraries'),
+    ],
+  },
+  {
+    title: 'Development',
+    pages: [
+      new PageLink('Get Started', 'develop'),
+      new ExternalLink('Browse Git', 'https://github.com/PDLPorters/pdl'),
+      new ExternalLink('Bugs', 'https://github.com/PDLPorters/pdl/issues'),
+      new ExternalLink('Pull Requests', 'https://github.com/PDLPorters/pdl/pulls'),
+      new PageLink('Credits', 'credits'),
+    ],
+  },
+];
